b465b91ed631a5cc5744a98d57345402333d5716
mr._ak__02 <294447718+ameshwardewangan-sys@users.noreply.github.com>
2026-06-27 21:33:36 +0530

    Export AI tutor functions from ai-tutor module

---
+++ functions/ai-tutor.js
@@ -0,0 +1,6 @@
+const aiTutor = require("./ai-tutor");
+
+exports.askAI = aiTutor.askAI;
+exports.generateQuiz = aiTutor.generateQuiz;
+exports.evaluateAnswer = aiTutor.evaluateAnswer;
+exports.generateSummary = aiTutor.generateSummary;
