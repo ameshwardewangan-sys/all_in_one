27486398deb8d730b7de48f66d0b35e0845c52bf
mr._ak__02 <294447718+ameshwardewangan-sys@users.noreply.github.com>
2026-06-27 20:18:31 +0530

    Add Firebase structure for users, questions, and analytics

---
+++ firebase.js
@@ -0,0 +1,37 @@
+users/
+  userId/
+    name
+    email
+    coins
+    xp
+    level
+    selectedExams
+    progress
+    lastActive
+
+questions/
+  examId/
+    questionId/
+      question
+      options
+      answer
+      explanation
+
+vacancies/
+  vacancyId/
+    title
+    applyLink
+    lastDate
+    state
+    examType
+
+weather_cache/
+  city/
+    temp
+    condition
+    updatedAt
+
+analytics/
+  totalUsers
+  activeUsers
+  downloads
